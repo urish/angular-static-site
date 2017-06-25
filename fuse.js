97a01d46a40edc082b52ac99cffa29c222c1fe3e
Uri Shaked <uri@salsa4fun.co.il>
2017-06-25 08:21:28 +0300

    style: single quote in fuse config

--- fuse.js
+++ fuse.js
@@ -1,19 +1,19 @@
 const { FuseBox, TypeScriptHelpers } = require('fuse-box');
 
 const fuse = FuseBox.init({
-	homeDir: `src/`,
-	output: `dist/$name.js`,
+	homeDir: 'src/',
+	output: 'dist/$name.js',
 	plugins: [ TypeScriptHelpers() ]
 });
 
 fuse.dev({ port: 4445 });
 
-fuse.bundle("vendor")
+fuse.bundle('vendor')
 	.hmr()
-	.instructions(" ~ main.ts");
+	.instructions(' ~ main.ts');
 
-fuse.bundle("app")
+fuse.bundle('app')
 	.sourceMaps(true)
-	.instructions(" !> [main.ts]").watch().hmr();
+	.instructions(' !> [main.ts]').watch().hmr();
 
 fuse.run();
