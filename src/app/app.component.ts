3dc98544965517dfcc4f4963700e2bf9b548981c
Uri Shaked <uri@salsa4fun.co.il>
2017-12-07 15:51:35 +0100

    feat: preboot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: '<h1>Hello, {{name}}!</h1>'
+    template: `
+        <h1>Hello, {{name}}!</h1>
+        Enter your name: <input [(ngModel)]="name" />
+    `
 })
 export class AppComponent {
     name = 'Angular';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser';
+import { PrebootModule } from 'preboot';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule.withServerTransition({ appId: 'universal-app' })],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'universal-app' }),
+        PrebootModule.withConfig({ appRoot: 'app-root' }),        
+        FormsModule,
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
